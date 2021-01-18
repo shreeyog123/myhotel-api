7a1038bab8cd5afcaff6cd699fed14b4ac148e28
Shreeyog Nimkar <Shreeyog_Nimkar@epam.com>
2021-01-18 22:42:17 +0530

    Initial commit

---
+++ myhotel-api/src/main/java/com/myhotel/myhotel/api/MyhotelApiApplication.java
@@ -0,0 +1,17 @@
+package com.myhotel.myhotel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class MyhotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhotelApiApplication.class, args);
+	}
+
+}
